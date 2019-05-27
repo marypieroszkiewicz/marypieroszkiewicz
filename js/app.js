681319771c278693c1a38c6aaf1fd501365db559
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2019-05-27 23:19:01 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
 	let anchorlinks = document.querySelectorAll('a[href^="#"]');
 
-	for ( let item of anchorlinks) { // relitere 
-		item.addEventListener('click', (e)=> {
+	for (let item of anchorlinks) { // relitere
+		item.addEventListener('click', (e) => {
 			let hashval = item.getAttribute('href');
 			let target = document.querySelector(hashval);
 			target.scrollIntoView({
 				behavior: 'smooth'
 			});
-			
+
 			history.pushState(null, null, hashval);
 			e.preventDefault();
 		});
